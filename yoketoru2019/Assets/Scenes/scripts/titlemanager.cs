7b76371e63206b05ce727306b5fa31268909aa4b
ozeki <hhdfgg@users.noreply.github.com>
2019-06-28 15:45:12 +0900

    yoketoru

---
+++ yoketoru2019/Assets/Scenes/scripts/titlemanager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class titlemanager : MonoBehaviour
+{
+
+    bool sceneChanged = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sceneChanged == true) return;
+
+        if(Input.GetMouseButtonDown(0)||Input.GetButtonDown("Jump"))
+        {
+            sceneChanged = true;
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game");
+        }
+    }
+}
